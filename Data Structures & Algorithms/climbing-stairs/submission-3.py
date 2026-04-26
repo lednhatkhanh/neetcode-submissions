280ea0939e15daab7966c7a550ba1c1b9e076182
Nhat Khanh <lednhatkhanh@gmail.com>
2026-04-26 22:58:47 +0700

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,15 @@
+from functools import cache
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n < 3:
+            return n
+        
+        n1, n2 = 2, 3
+        
+        for i in range(4, n + 1):
+            curr = n1 + n2
+            n1 = n2
+            n2 = curr
+        
+        return n2
